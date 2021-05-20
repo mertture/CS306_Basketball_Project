ca38093e2c28259d903c95eb6843e3241353faa5
Mert Türe <mertture0@gmail.com>
2021-05-20 17:47:37 +0300

    TeamStandingsTemplate

--- config.php
+++ config.php
@@ -5,4 +5,19 @@ if ($db->connect_errno > 0) {
     die('Unable to connect to database [' . $db->connect_error. ']');
 }
 
+
+$attributes = [
+    "tid" => "Team ID",
+    "player_id" => "Player ID",
+    "home_win" => "Home Wins",
+    "home_loses" => "Home Loses",
+    "away_win" => "Away Wins",
+    "away_loses" => "Away Loses",
+    "s_year" => "Season",
+    "name" => "Team",
+    "total_scored" => "Scored Points",
+    "standing" => "Standing"
+];
+
+
 ?>
